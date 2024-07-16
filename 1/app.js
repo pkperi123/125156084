db2b7540e80f02bf3110fbe37c64da07231df0c4
Periakaruppan <pkperi1210@gmail.com>
2024-07-16 19:19:48 +0530

    first commit

---
+++ 1/app.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const axios = require('axios');
+
+
+const app = express();
+
+app.get('/', (req, res) => {
+    const response = axios.get()
+
+    
+
+
+app.listen(3000, () => {
+    console.log('Server is running on port 3000');
+});
